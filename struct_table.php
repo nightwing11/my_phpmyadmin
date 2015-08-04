40b6ffdd7a9f5023170994fbaba62f961a1e2b89
Jean-Gabriel <gontar_j@etna-alternance.net>
2015-08-04 18:08:09 +0200

    Adding choice to delete or update

--- struct_table.php
+++ struct_table.php
@@ -6,32 +6,44 @@ $tablename = htmlspecialchars($_GET["tablename"]);
 // $struct_db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 $struct_db = new PDO('mysql:host=localhost;dbname=' . $dbname . ';', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
-$describe_table = "DESCRIBE $tablename";
-$rep_describe = $struct_db->query($describe_table);
+if ( !isset($_GET["action"]) ) {
 
-echo '<table>';
-    echo '<th>Champ</th>';
-    echo '<th>Type/Valeur</th>';
-    echo '<th>Null</th>';
-    echo '<th>Action</th>';
+    $describe_table = "DESCRIBE $tablename";
+    $rep_describe = $struct_db->query($describe_table);
 
-    while ($data = $rep_describe->fetchAll()) {    
-        
-        $nb_datas = count($data);
-        for ( $a = 0; $a < $nb_datas; $a++ ) {
-            echo '<tr>';
-                echo '<td>' . $data[$a][0] . '</td>';
-                echo '<td>' . $data[$a][1] . '</td>';
-                echo '<td>' . $data[$a][2] . '</td>';
-                echo '<td>';
-                    echo '<a href="" class="ope-actions">Modifier</a>';
-                    echo '<a href="" class="ope-actions">Supprimer</a>';
-                echo'</td>';
-            echo '</tr>';
+    echo '<table>';
+        echo '<th>Champ</th>';
+        echo '<th>Type/Valeur</th>';
+        echo '<th>Null</th>';
+        echo '<th>Action</th>';
+
+        while ($data = $rep_describe->fetchAll()) {    
+
+            $nb_datas = count($data);
+            for ( $a = 0; $a < $nb_datas; $a++ ) {
+                echo '<tr>';
+                    echo '<td>' . $data[$a][0] . '</td>';
+                    echo '<td>' . $data[$a][1] . '</td>';
+                    echo '<td>' . $data[$a][2] . '</td>';
+                    echo '<td>';
+                        echo '<a href="dashboard.php?dbname=' . $dbname . '&tablename=' . $tablename . '&structure=0&action=modifier" class="ope-actions">Modifier</a>';
+                        echo '<a href="dashboard.php?dbname=' . $dbname . '&tablename=' . $tablename . '&structure=0&action=supprimer" class="ope-actions">Supprimer</a>';
+                    echo'</td>';
+                echo '</tr>';
+            }
+
         }
 
-    }
+    echo '</table>';
 
-echo '</table>';
+    $rep_describe->closeCursor();
 
-$rep_describe->closeCursor();
+}
+
+if ( isset($_GET["action"]) && $_GET["action"] == "modifier" ) {
+    echo 'Faire le formulaire de modification';
+}
+
+if ( isset($_GET["action"]) && $_GET["action"] == "supprimer" ) {
+    echo 'Faire le formulaire de suppression';
+}
