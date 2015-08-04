ee5026d6874f00db0fba75f41608b3b1dcc51650
mrldm <benlal_m@etna-alternance.net>
2015-08-04 15:48:32 +0200

    import .sql only
    une base de test de test pour tester

--- import.php
+++ import.php
@@ -1,44 +1,74 @@
 <?php
-
+// connexion à la base 
     try {
-      		$cnx = new PDO('mysql:host=localhost;', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            $cnx = new PDO('mysql:host=localhost;', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+      		
       		//$cnx = new PDO('mysql:host=localhost;', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
      	}
     catch (PDOException $dbex) {
      	   die("Erreur de connexion : " . $dbex->getMessage() );
       	}
 
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["file"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    //variable fichier d'uplaod
+    $target_dir = "uploads/"; // dossier d'uplaoads
+    $target_file = $target_dir . basename($_FILES["file"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
     //taille du fichier
     if ($_FILES["file"]["size"] > 2048000) {
-        echo "Sorry, your file is too large.";
+        echo "Désolé ton fichier est trop grand</br>";
         $uploadOk = 0;
     }
 
     //vérifie le format du fichier 
     if($imageFileType != "gzip" && $imageFileType != "bzip2" && $imageFileType != "zip"
     	 && $imageFileType != "sql" ) {
-         echo "Sorry, only gzip, bzip2, zip & sql files are allowed.";
+         echo "Seulement gzip, bzip2, zip & sql sont autorisés.</br>";
         $uploadOk = 0;
     }
-    echo $uploadOk;
+
     //Upload 
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
+        echo "Désolé, votre fichier ne s'est pas chargé </br>";
     } else {
         if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
+            echo "Le fichier ". basename( $_FILES["file"]["name"]). " s'est chargé </br>";
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            echo "</br>Erreur de chargement du fichier";
         }
     }
 
+    //le nom du fichier 
+    $filename = $_FILES["file"]["name"];
+    echo $filename;
+
+    $monfichier = '';
+
+    //ouverture du fichier et lecture 
+    $monfichier = file_get_contents("uploads/$filename");
 
 
-// on exécute
-$cnx->exec($tonfichier);
+      if($monfichier == false)
+        echo "</br> erreur de lecture";
+      else 
+         {   
+            //$monfichier=str_replace("\n","",$monfichier);
+            //$monfichier=str_replace("\r","",$monfichier);
+            $res = $cnx->exec($monfichier);
+            if($res == false) 
+                {
+                     echo "</br>\nPDO::errorInfo():\n";
+                     print_r($cnx->errorInfo());
+                   //die(print_r($res->errorInfo(), true);
+                }
+            else  
+                echo "</br>la requete s'est exécuté avec succès";
+         }
+
+    header('dashboard.php?importer=0');
+     //on ferme tout
+    close($monfichier);
+    $cnx->closeCursor();
+    
 ?>
